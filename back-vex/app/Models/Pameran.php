e91cdf78977133e0f96aed73c33a73002d385ca0
devika <devikah.7b@gmail.com>
2026-05-29 10:37:36 +0700

    mengisi model pameran

--- back-vex/app/Models/Pameran.php
+++ back-vex/app/Models/Pameran.php
@@ -6,5 +6,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pameran extends Model
 {
-    //
+    public $timestamps = false;
+    protected $table   = 'pameran';
+    protected $primaryKey = 'id_pameran';
+
+    protected $fillable = [
+        'model_pameran',
+        'kategori',
+        'banner',
+        'judul',
+        'deskripsi',
+        'kapasitas',
+        'tanggal_mulai',
+        'tanggal_akhir',
+        'tanggal_mulai_persiapan',
+        'tanggal_akhir_persiapan',
+    ];
+
+    // Relasi ke tabel model (aset 3D)
+    public function model3d()
+    {
+        return $this->belongsTo(ModelPameran::class, 'model_pameran', 'id_model');
+    }
+
+    // Relasi ke tabel prodi
+    public function prodi()
+    {
+        return $this->belongsTo(Prodi::class, 'kategori', 'kode_prodi');
+    }
 }
