bcc717662bf3147c65d7968e43450333e2122c07
devika <devikah.7b@gmail.com>
2026-06-03 14:53:23 +0700

    mengisi model

--- back-vex/app/Models/Karya.php
+++ back-vex/app/Models/Karya.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Karya extends Model
 {
-    //
+    public $timestamps    = false;
+    protected $table      = 'karya';
+    protected $primaryKey = 'id_karya';
+
+    protected $fillable = [
+        'id_pengguna',
+        'id_stan',
+        'judul',
+        'deskripsi',
+        'tautan',
+        'gambar_poster',
+        'gambar_sampul',
+    ];
+
+    // Relasi ke pengguna (Ketua PBL)
+    public function pengguna()
+    {
+        return $this->belongsTo(Pengguna::class, 'id_pengguna', 'id');
+    }
+
+    // Relasi ke stan
+    public function stan()
+    {
+        return $this->belongsTo(Stan::class, 'id_stan', 'id_stan');
+    }
 }
