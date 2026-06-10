dacf69f148c9834674faa3f1b97989129cf11de8
Graaph <truebadluck146@gmail.com>
2026-06-10 20:06:04 +0700

    Fix group function experience

--- back-vex/routes/api.php
+++ back-vex/routes/api.php
@@ -37,13 +37,13 @@ Route::get('/karya-terbaik', [KpsController::class, 'karyaTerbaik']); // ✅ pub
 // !! JANGAN DISENTUH !!
 //-----------------------
 
-Route::prefix('experience')->group(function()){
+Route::prefix('experience')->group(function(){
     Route::get('/game-assets', [GameAssetController::class, 'index']);
     //Butuh route untuk ambil 3D model dari setiap booth
     //Butuh route untuk ambil data karya setiap booth (data karyanya beserta komentar likes dan penilaiannya)
     //Butuh route untuk ambil 3D model pameran
 
-}
+});
 
 // =============================
 // PROTECTED ROUTES
